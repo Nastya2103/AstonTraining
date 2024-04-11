3048101b24173b3da0d3a5196e478bd7db5de724
Nastya2103 <99600134+Nastya2103@users.noreply.github.com>
2024-04-11 22:14:03 +0300

    Домашнее задание 12 junit

---
+++ src/main/java/org/example/CalculatorFactorial.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class CalculatorFactorial {
+    public static int calculateFactorial(int num) {
+        int result = 1;
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
